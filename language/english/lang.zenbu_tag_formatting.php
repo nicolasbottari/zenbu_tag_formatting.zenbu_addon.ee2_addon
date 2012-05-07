ae8a9dbf84333765dc30d83adfa3865946636ae1
Nicolas Bottari <nicolasbottari@gmail.com>
2012-05-07 16:01:49 -0400

    Initial commit

---
+++ language/english/lang.zenbu_tag_formatting.php
@@ -0,0 +1,22 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+*	===============================
+*	LANGUAGE FILE
+*	===============================
+*
+*/
+
+$lang = array(
+
+'option' => 'Option',
+
+'separator' => 'Separate tags by: ',
+
+'comma' => 'Comma',
+
+'linebreak' => 'Line break &lt;br /&gt;', 
+
+//
+''=>''
+);
